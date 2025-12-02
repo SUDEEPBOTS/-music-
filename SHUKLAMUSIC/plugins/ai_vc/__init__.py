65e7db53d09ba909b0f3987e55d1083453032c84
𝐒𝐔𝐃𝐄𝐄𝐏𝐁𝐎𝐓𝐒 <sudeeptelegram@gmail.com>
2025-12-02 14:28:02 +0530

    Update __init__.py

--- SHUKLAMUSIC/plugins/ai_vc/__init__.py
+++ SHUKLAMUSIC/plugins/ai_vc/__init__.py
@@ -1,1 +1,9 @@
 """AI VC helper package."""
+
+from .ai_emotion import detect_emotion
+from .ai_gemini import ask_gemini
+from .ai_gf_gemini import ask_gf_gemini
+from .ai_human_tts import synthesize_human_voice
+from .tts_engine import synthesize_speech
+from .stt_whisper import whisper_to_text
+from .vc_gf_handler import *
